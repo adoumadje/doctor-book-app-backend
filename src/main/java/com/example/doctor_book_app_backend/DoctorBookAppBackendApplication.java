98037b9d4b18efb07b0900250a36bc368f9aa3c2
adoumadje <adoumadjecaleb@gmail.com>
2025-01-10 01:15:28 +0100

    Automatic add admin

--- src/main/java/com/example/doctor_book_app_backend/DoctorBookAppBackendApplication.java
+++ src/main/java/com/example/doctor_book_app_backend/DoctorBookAppBackendApplication.java
@@ -1,8 +1,13 @@
 package com.example.doctor_book_app_backend;
 
+import com.example.doctor_book_app_backend.entity.Admin;
+import com.example.doctor_book_app_backend.repository.AdminRepository;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @SpringBootApplication
 @ConfigurationPropertiesScan
@@ -10,6 +15,22 @@ public class DoctorBookAppBackendApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DoctorBookAppBackendApplication.class, args);
+	}
+
+	@Bean
+	public CommandLineRunner commandLineRunner(AdminRepository adminRepository,
+											   PasswordEncoder passwordEncoder) {
+		return runner -> {
+			createAdmin(adminRepository, passwordEncoder);
+		};
+	}
+
+	private void createAdmin(AdminRepository adminRepository,
+							 PasswordEncoder passwordEncoder) {
+		adminRepository.save(new Admin().builder()
+				.username("Admin")
+				.password(passwordEncoder.encode("AdminPa55"))
+				.build());
 	}
 
 }
--- src/main/java/com/example/doctor_book_app_backend/entity/Admin.java
+++ src/main/java/com/example/doctor_book_app_backend/entity/Admin.java
@@ -1,11 +1,17 @@
 package com.example.doctor_book_app_backend.entity;
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Entity
 @Table(name = "admin")
 @Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class Admin {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
